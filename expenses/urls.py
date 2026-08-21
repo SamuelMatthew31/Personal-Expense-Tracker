db9e2309874e7eb5d85f3383bef98db16d07cbef
Samuel Matthew <samuelsamadikun6@gmail.com>
2026-08-21 11:56:36 +0700

    2nd Project: Creating dashboard template UI for user to be redirect after login

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, dashboard
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('dashboard/', dashboard, name='dashboard'),
+
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 # Create your views here.
+@login_required
+def dashboard(request):
+    return render(request, 'expenses/dashboard.html')
+
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
